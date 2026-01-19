c9e354cf9eb77645c62101c6e000ae731d7bb5f0
earlgrey02 <san06036@naver.com>
2026-01-19 17:57:19 +0900

    feat: 예외 관련 테스트 유틸리티 추가

--- api/src/testFixtures/kotlin/com/recap/api/util/WebTestClientUtils.kt
+++ api/src/testFixtures/kotlin/com/recap/api/util/WebTestClientUtils.kt
@@ -1,5 +1,6 @@
 package com.recap.api.util
 
+import com.recap.api.global.dto.ErrorResponse
 import io.kotest.matchers.shouldBe
 import org.springframework.http.HttpStatus
 import org.springframework.test.web.reactive.server.WebTestClient.BodySpec
@@ -13,3 +14,5 @@ fun ResponseSpec.expectStatus(status: HttpStatus): ResponseSpec =
 inline fun <reified T : Any> ResponseSpec.expectBody(body: T): BodySpec<T, *> =
     expectBody<T>()
         .consumeWith { it.responseBody shouldBe body }
+
+fun ResponseSpec.expectError(): BodySpec<ErrorResponse, *> = expectBody<ErrorResponse>()
