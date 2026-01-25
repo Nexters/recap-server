02cde1f01b90c396ae7c028ec9cdf7269fede00f
earlgrey02 <san06036@naver.com>
2026-01-26 01:11:19 +0900

    feat: JPA 설정

---
+++ core/src/main/kotlin/com/recap/core/global/config/JpaConfiguration.kt
@@ -0,0 +1,12 @@
+package com.recap.core.global.config
+
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
+@Configuration
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = ["com.recap.core.domain"])
+@EntityScan(basePackages = ["com.recap.core.domain"])
+class JpaConfiguration
