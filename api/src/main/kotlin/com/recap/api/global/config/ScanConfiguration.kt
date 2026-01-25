d03cab6e85bc8adf7484927104c6b6050714fac4
earlgrey02 <san06036@naver.com>
2026-01-26 01:10:16 +0900

    feat: 프로퍼티 스캔 설정

--- api/src/main/kotlin/com/recap/api/global/config/ScanConfiguration.kt
+++ api/src/main/kotlin/com/recap/api/global/config/ScanConfiguration.kt
@@ -1,8 +1,10 @@
 package com.recap.api.global.config
 
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@ConfigurationPropertiesScan(basePackages = ["com.recap.core"])
 @ComponentScan(basePackages = ["com.recap.api", "com.recap.core"])
 class ScanConfiguration
