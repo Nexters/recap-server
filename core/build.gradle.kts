b4d9710349239d8ca3811a398394a69df671988c
earlgrey02 <san06036@naver.com>
2026-01-26 01:10:28 +0900

    feat: RestClient 설정

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -9,6 +9,7 @@ allOpen {
 }
 
 dependencies {
+    implementation(libs.spring.web)
     implementation(libs.spring.data.jpa)
     implementation(libs.hypersistence.utils)
     implementation(libs.bundles.jwt)
