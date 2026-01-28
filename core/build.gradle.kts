09c90afa8d449a54953771de71dc7c13021ac35c
earlgrey02 <san06036@naver.com>
2026-01-28 18:51:26 +0900

    feat: Redis 관련 의존성 추가

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -11,6 +11,7 @@ allOpen {
 dependencies {
     implementation(libs.spring.web)
     implementation(libs.spring.data.jpa)
+    implementation(libs.spring.data.redis)
     implementation(libs.hypersistence.utils)
     implementation(libs.bundles.jwt)
     runtimeOnly(libs.mysql.connector)
