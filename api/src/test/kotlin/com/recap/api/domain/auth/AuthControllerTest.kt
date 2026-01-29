6e942e67a103c49778599e83d0459ae0b34dd900
earlgrey02 <san06036@naver.com>
2026-01-29 18:01:08 +0900

    refactor: 로그인 테스트 코드 리팩토링

--- api/src/test/kotlin/com/recap/api/domain/auth/AuthControllerTest.kt
+++ api/src/test/kotlin/com/recap/api/domain/auth/AuthControllerTest.kt
@@ -11,6 +11,7 @@ import com.recap.api.snippet.loginResponseFields
 import com.recap.api.util.document
 import com.recap.api.util.expectBody
 import com.recap.api.util.expectError
+import com.recap.api.util.expectStatus
 import com.recap.core.domain.auth.exception.InvalidOAuthTokenException
 import com.recap.core.domain.auth.service.AuthService
 import com.recap.core.fixture.createLoginResult
@@ -24,22 +25,22 @@ class AuthControllerTest : ControllerTest() {
 
     init {
         describe("login()은") {
-            val uri = "/api/v1/auth/login"
-            val request = createLoginRequest()
-            val result = createLoginResult()
+            val request =
+                webClient
+                    .post()
+                    .uri("/auth/login")
+                    .bodyValue(createLoginRequest())
 
-            context("유효한 OAuth2 토큰이 주어진 경우") {
-                every { authService.login(request.toCommand()) } returns result
+            context("유효한 요청이 주어진 경우") {
+                val result = createLoginResult()
+
+                every { authService.login(any()) } returns result
 
                 it("상태 코드 200과 LoginResponse를 반환한다.") {
-                    webClient
-                        .post()
-                        .uri(uri)
-                        .bodyValue(request)
+                    request
                         .exchange()
-                        .expectStatus()
-                        .isOk
-                        .expectBody<LoginResponse>(LoginResponse.from(result))
+                        .expectStatus(200)
+                        .expectBody(LoginResponse.from(result))
                         .document("로그인 성공(200)") {
                             requestBody(loginRequestFields)
                             responseBody(loginResponseFields)
@@ -48,16 +49,12 @@ class AuthControllerTest : ControllerTest() {
             }
 
             context("유효하지 않은 OAuth2 토큰이 주어진 경우") {
-                every { authService.login(request.toCommand()) } throws InvalidOAuthTokenException()
+                every { authService.login(any()) } throws InvalidOAuthTokenException()
 
                 it("상태 코드 401과 ErrorResponse를 반환한다.") {
-                    webClient
-                        .post()
-                        .uri(uri)
-                        .bodyValue(request)
+                    request
                         .exchange()
-                        .expectStatus()
-                        .isUnauthorized
+                        .expectStatus(401)
                         .expectError()
                         .document("로그인 실패(401)") {
                             requestBody(loginRequestFields)
