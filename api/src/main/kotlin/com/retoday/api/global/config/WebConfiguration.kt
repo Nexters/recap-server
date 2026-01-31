959e13ff896e9777b501af7a429e983a490fe58c
earlgrey02 <san06036@naver.com>
2026-01-31 22:01:05 +0900

    feat: CORS 설정

---
+++ api/src/main/kotlin/com/retoday/api/global/config/WebConfiguration.kt
@@ -0,0 +1,21 @@
+package com.retoday.api.global.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfiguration(
+    @Value("\${web.uri}")
+    private val webUri: String
+) : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry
+            .addMapping("/api/**")
+            .allowedOrigins(webUri)
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+    }
+}
