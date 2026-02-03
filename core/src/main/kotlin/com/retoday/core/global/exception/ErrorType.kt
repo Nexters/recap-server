4f7f04c485ce61942f9b767184b015c5a62f1984
earlgrey02 <san06036@naver.com>
2026-02-03 15:57:23 +0900

    feat: 예외 관련 열거체 구현

---
+++ core/src/main/kotlin/com/retoday/core/global/exception/ErrorType.kt
@@ -0,0 +1,22 @@
+package com.retoday.core.global.exception
+
+enum class ErrorType(
+    val message: String,
+    val status: Int
+) {
+    // Global
+    INTERNAL_SERVER_ERROR("서버 오류가 발생했습니다.", 500),
+    INVALID_REQUEST("잘못된 요청입니다.", 400),
+    UNAUTHENTICATED("인증되지 않은 사용자입니다.", 401),
+    UNAUTHORIZED("인가되지 않은 사용자입니다.", 403),
+
+    // Auth
+    INVALID_AUTHENTICATION_TOKEN("유효하지 않은 인증 토큰입니다.", 400),
+    INVALID_OAUTH_TOKEN("유효하지 않은 OAuth2 토큰입니다.", 400),
+    REFRESH_TOKEN_NOT_FOUND("존재하지 않는 리프레시 토큰입니다.", 404),
+
+    // User
+    USER_NOT_FOUND("존재하지 않는 사용자입니다.", 404);
+
+    val code: String = name
+}
