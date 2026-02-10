ed73965dfe48977011cc28ae687c940a9517d6bb
이예림 <lee_yerim@naver.com>
2026-02-10 16:14:57 +0900

    chore: BatchResult 파일 삭제

--- api/src/main/kotlin/com/retoday/api/domain/history/dto/response/HistoryRecordBatchResponse.kt
+++ api/src/main/kotlin/com/retoday/api/domain/history/dto/response/HistoryRecordBatchResponse.kt
@@ -1,19 +1,19 @@
 package com.retoday.api.domain.history.dto.response
 
-import com.retoday.core.domain.history.dto.result.BatchResult
+import com.retoday.core.domain.history.dto.result.BatchItemResult
 import com.retoday.core.domain.history.dto.result.HistoryRecordBatchResult
 
 data class HistoryRecordBatchResponse(
     val successCount: Int,
     val failedCount: Int,
-    val results: List<BatchResult>
+    val results: List<BatchItemResult>
 ) {
     companion object {
         fun from(result: HistoryRecordBatchResult): HistoryRecordBatchResponse =
             HistoryRecordBatchResponse(
                 successCount = result.successCount,
                 failedCount = result.failedCount,
-                results = result.results.map { BatchResult.from(it) }
+                results = result.results
             )
     }
 }
