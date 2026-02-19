8229025583652ca8a9697bd80cd8b988b35e21df
earlgrey02 <san06036@naver.com>
2026-02-19 23:42:42 +0900

    feat: 웹사이트 집계 조회 쿼리에 날짜 제한 조건 추가

--- core/src/main/kotlin/com/retoday/core/domain/history/repository/HistoryRepository.kt
+++ core/src/main/kotlin/com/retoday/core/domain/history/repository/HistoryRepository.kt
@@ -37,8 +37,8 @@ interface HistoryRepository : JpaRepository<History, Long> {
             JOIN website w ON w.id = h.website_id
             LEFT JOIN website_category wc ON wc.id = w.category_id
             WHERE h.user_id = :userId
-              AND h.visited_at < :endedAt
-              AND h.closed_at > :startedAt
+              AND h.visited_at BETWEEN DATE_SUB(:startedAt, INTERVAL 1 DAY) AND :endedAt
+              AND h.closed_at BETWEEN :startedAt AND DATE_ADD(:endedAt, INTERVAL 1 DAY)
             GROUP BY h.website_id, w.domain, w.favicon_url, wc.name
             ORDER BY stayDuration DESC
             """,
