1c7c4d243b30ca9b1677dbbbf80c849075ef575f
earlgrey02 <san06036@naver.com>
2026-02-19 23:59:15 +0900

    fix: 잘못된 인덱스 수정

--- core/src/main/kotlin/com/retoday/core/domain/history/entity/History.kt
+++ core/src/main/kotlin/com/retoday/core/domain/history/entity/History.kt
@@ -2,14 +2,17 @@ package com.retoday.core.domain.history.entity
 
 import com.retoday.core.global.entity.BaseEntity
 import io.hypersistence.utils.hibernate.id.Tsid
-import jakarta.persistence.*
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Index
+import jakarta.persistence.Table
 import java.time.Instant
 import java.time.LocalDate
 
 @Entity
 @Table(
     indexes = [
-        Index(name = "idx_user_date", columnList = "user_id, visited_date")
+        Index(name = "idx_user_id_visited_at", columnList = "user_id, visited_at")
     ]
 )
 class History(
