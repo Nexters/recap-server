6c2cd756de137bad1628c1445bc455c8fd4827a5
earlgrey02 <san06036@naver.com>
2026-02-24 23:30:06 +0900

    refactor: 도메인 유효성 검사 어노테이션 추가

--- api/src/main/kotlin/com/retoday/api/domain/user/dto/request/DeleteMyExcludedDomainRequest.kt
+++ api/src/main/kotlin/com/retoday/api/domain/user/dto/request/DeleteMyExcludedDomainRequest.kt
@@ -1,15 +1,12 @@
 package com.retoday.api.domain.user.dto.request
 
+import com.retoday.api.global.validation.Domain
 import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.Pattern
 import jakarta.validation.constraints.Size
 
 data class DeleteMyExcludedDomainRequest(
     @field:NotBlank
     @field:Size(max = 255)
-    @field:Pattern(
-        regexp = "^(?!-)(?:[a-zA-Z0-9-]{1,63}\\.)+[a-zA-Z]{2,63}$",
-        message = "유효한 도메인 형식이 아닙니다."
-    )
+    @field:Domain
     val domain: String
 )
